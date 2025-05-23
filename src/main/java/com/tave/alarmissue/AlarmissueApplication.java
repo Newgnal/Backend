cd5d6da6eacf1cb87cfdf7b453dbdcdd653fc22a
limdodod <rosalim2001@naver.com>
2025-05-23 23:30:30 +0900

    initial commit

---
+++ src/main/java/com/tave/alarmissue/AlarmissueApplication.java
@@ -0,0 +1,13 @@
+package com.tave.alarmissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmissueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmissueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tave/alarmissue/AlarmissueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tave.alarmissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmissueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
