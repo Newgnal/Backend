3870f25d7b247b8b809e248302697771be56dca7
임도현 <162318258+limdodod@users.noreply.github.com>
2025-06-18 21:32:57 +0900

    feat: kakoLogin 구현 (#2)
    * feat(login): 카카오 로그인 구현
    
    * feat(auth): 회원 가입 시 닉네임 랜덤 생성
    
    * fix: application-oauth 추가
    
    * refractor(auth): 경로 수정
    
    * refractor(auth): 회원가입 로직 수정
    
    * fix(Auth): 응답헤더 포맷 수정"
    
    * fix(user): user엔티티 수정
    
    * fix(Auth): 오타 수정
    
    * fix: authentication userId반환-> email 반환
    
    * bug(auth): 로그인 에러 수정
    
    * fix(auth): delete unused file
    
    * feat(auth): 프론트 반환용 헤더 추가
    
    * refractor(auth): 엑세스 토큰 발급 로직 수정

---
+++ src/main/java/com/tave/alarmissue/auth/service/AuthService.java
@@ -0,0 +1,108 @@
+package com.tave.alarmissue.auth.service;
+
+import com.tave.alarmissue.auth.client.KakaoApiClient;
+import com.tave.alarmissue.auth.converter.AuthConverter;
+import com.tave.alarmissue.auth.dto.response.JwtLoginResponse;
+import com.tave.alarmissue.auth.dto.response.KakaoUserInfo;
+import com.tave.alarmissue.redis.entity.RefreshToken;
+import com.tave.alarmissue.redis.service.RefreshTokenRedisService;
+import com.tave.alarmissue.security.exception.SecurityErrorCode;
+import com.tave.alarmissue.security.exception.TokenException;
+import com.tave.alarmissue.security.jwt.JwtProvider;
+import com.tave.alarmissue.user.domain.UserEntity;
+import com.tave.alarmissue.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthService {
+
+    private final KakaoApiClient kakaoApiClient;
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+    private final RefreshTokenRedisService refreshTokenRedisService;
+
+    @Value("${jwt.expiration.refresh}")
+    private Long REFRESH_TOKEN_EXPIRE_TIME;
+
+    public JwtLoginResponse loginOrRegister(String code) {
+
+        // 1. 인가 코드로 카카오 access token 요청
+        String kakaoAccessToken = kakaoApiClient.requestAccessToken(code);
+
+        // 2. 카카오 access token으로 사용자 정보 조회
+        KakaoUserInfo kakaoUserInfo = kakaoApiClient.getUserInfo(kakaoAccessToken);
+        String email = kakaoUserInfo.getKakaoAccount().getEmail();
+        String profileImage = kakaoUserInfo.getKakaoAccount().getProfile().getProfileImageUrl();
+
+
+        // 3. 회원 조회 또는 생성
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseGet(() -> {
+                    String nicknameToUse = generateRandomNickname();
+                    return userRepository.save(UserEntity.builder()
+                            .email(email)
+                            .nickName(nicknameToUse)
+                            .imageUrl(profileImage)
+                            .enabled(false)
+                            .build());
+                });
+
+        // 4. Access & Refresh Token 생성
+        String userIdStr = user.getId().toString();
+        Authentication authentication = jwtProvider.getAuthenticationFromUserId(userIdStr);
+        String accessToken = jwtProvider.generateAccessToken(authentication, userIdStr);
+        String refreshToken = jwtProvider.generateRefreshToken(authentication, userIdStr);
+
+
+        // 5. Refresh Token을 Redis에 저장
+        refreshTokenRedisService.saveRefreshToken(user.getId().toString(), refreshToken, REFRESH_TOKEN_EXPIRE_TIME);
+
+        // 6. JwtLoginResponse 반환
+        return AuthConverter.toJwtLoginResponse(user,accessToken,refreshToken);
+    }
+
+    //닉네임 unique 처리
+    private String generateRandomNickname() {
+        for (int i = 0; i < 5; i++) {
+            String nickname = "user_" + UUID.randomUUID().toString().substring(0, 4);
+            boolean exists = userRepository.existsByNickName(nickname);
+            if (!exists) {
+                return nickname;
+            }
+        }
+        // 최대 시도 횟수 초과 시, 그냥 UUID 전체를 씀
+        return "user_" + UUID.randomUUID().toString().replace("-", "");
+    }
+
+    //백엔드 확인용
+    public String reissueAccessToken(String refreshToken) {
+
+        // refreshToken 유효성 검사
+        if (!jwtProvider.validateToken(refreshToken)) {
+            throw new TokenException(SecurityErrorCode.REFRESH_EXPIRED);
+        }
+
+        // refreshToken에서 userId 추출
+        String userId = jwtProvider.getSubject(refreshToken);
+
+        // redis에서 refreshToken 검증
+        Optional<RefreshToken> optionalRefresh = refreshTokenRedisService.findRefreshToken(Long.parseLong(userId));
+
+        if (optionalRefresh.isEmpty() || !optionalRefresh.get().getRefreshToken().equals(refreshToken)) {
+            throw new TokenException(SecurityErrorCode.REFRESH_EXPIRED);
+        }
+
+        // 새 access token 생성
+        Authentication authentication = jwtProvider.getAuthenticationFromUserId(userId);
+        return jwtProvider.generateAccessToken(authentication, userId);
+    }
+}
