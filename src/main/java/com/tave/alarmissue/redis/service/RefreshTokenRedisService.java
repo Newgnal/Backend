446443f913e1e6a4a67a1ca3593b512f43078f10
임도현 <162318258+limdodod@users.noreply.github.com>
2025-06-25 20:44:25 +0900

    feat: 로그아웃 구현 (#5)
    * feat(auth): 로그아웃 구현
    
    * fix(Auth): 인증 로직 수정

--- src/main/java/com/tave/alarmissue/redis/service/RefreshTokenRedisService.java
+++ src/main/java/com/tave/alarmissue/redis/service/RefreshTokenRedisService.java
@@ -29,8 +29,11 @@ public class RefreshTokenRedisService {
         return refreshTokenRepository.findById(userId);
     }
 
-    public void deleteRefreshToken(Long userId) {
-        refreshTokenRepository.deleteById(userId);
+    public boolean deleteRefreshToken(Long userId) {
+        if (refreshTokenRepository.existsById(userId)) {
+            refreshTokenRepository.deleteById(userId);
+            return true;
+        }
+        return false;
     }
-
 }
---
+++ src/main/java/com/tave/alarmissue/user/service/UserService.java
@@ -0,0 +1,22 @@
+package com.tave.alarmissue.user.service;
+
+import com.tave.alarmissue.redis.service.RefreshTokenRedisService;
+import com.tave.alarmissue.user.dto.response.LogoutResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+
+    private final RefreshTokenRedisService refreshTokenRedisService;
+
+    public LogoutResponse logout(Long userId) {
+        boolean isDeleted = refreshTokenRedisService.deleteRefreshToken(userId);
+        String message = isDeleted ? "Logout successful" : "Logout failed: User not found";
+        return new LogoutResponse(message);
+    }
+
+}
