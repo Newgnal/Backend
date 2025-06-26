b6832d4b9b4891c120ce301a295dcd4eb68b22bc
임도현 <162318258+limdodod@users.noreply.github.com>
2025-06-26 19:36:45 +0900

    fix(errorCode): customError반환하도록 수정 (#17)

--- src/main/java/com/tave/alarmissue/global/dto/ResponseError.java
+++ src/main/java/com/tave/alarmissue/global/dto/ResponseError.java
@@ -1,10 +1,13 @@
 package com.tave.alarmissue.global.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ResponseError {
     private String path;
     private String messageDetail;
     private String errorDetail;
+
 }
