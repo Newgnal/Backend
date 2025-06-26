d6001c660c3cd4edd93eadb4ec9ae36ce67cd576
ddhi7 <tjsanf211323@gmail.com>
2025-06-27 04:42:03 +0900

    controller return 형식 수정

--- src/main/java/com/tave/alarmissue/fcm/controller/FcmController.java
+++ src/main/java/com/tave/alarmissue/fcm/controller/FcmController.java
@@ -29,42 +29,24 @@ public class FcmController {
     // 알림 전송
     @PostMapping("/notifications")
     @Operation(summary = "푸시 알림 전송", description = "특정 FCM 토큰으로 푸시 알림을 전송합니다.")
-    public  ResponseEntity<CommonResponse<FcmNotificationResponse>> sendNotificationByToken(@RequestBody FcmSendReqeust fcmSendDto) {
+    public  ResponseEntity<FcmNotificationResponse> sendNotificationByToken(@RequestBody FcmSendReqeust fcmSendDto) {
         FcmNotificationResponse response = fcmService.sendPushNotification(fcmSendDto);
-        return ResponseEntity.ok(
-                CommonResponse.<FcmNotificationResponse>builder()
-                        .status(200)
-                        .message("푸시 알림이 성공적으로 전송되었습니다.")
-                        .data(response)
-                        .build()
-        );
+        return ResponseEntity.ok(response);
     }
 
     // 토큰 등록/갱신
     @PostMapping("/tokens")
     @Operation(summary = "FCM 토큰 등록/갱신", description = "사용자의 FCM 토큰을 등록하거나 갱신합니다.")
-    public ResponseEntity<CommonResponse<FcmTokenResponse>>  registerToken(@RequestBody FcmTokenRequest dto) {
+    public ResponseEntity<FcmTokenResponse> registerToken(@RequestBody FcmTokenRequest dto) {
         FcmTokenResponse response =fcmTokenService.registerToken(dto.getUserId(), dto.getFcmToken());
-        return ResponseEntity.status(HttpStatus.CREATED).body(
-                CommonResponse.<FcmTokenResponse>builder()
-                        .status(201)
-                        .message("FCM 토큰이 성공적으로 등록되었습니다.")
-                        .data(response)
-                        .build()
-        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     // 토큰 삭제
     @DeleteMapping("/tokens/{fcmToken}")
     @Operation(summary = "FCM 토큰 삭제", description = "지정된 FCM 토큰을 삭제합니다.")
-    public ResponseEntity<CommonResponse<Void>>  deleteToken(@RequestParam String fcmToken) {
+    public ResponseEntity<Void> deleteToken(@RequestParam String fcmToken) {
       fcmTokenService.deleteToken(fcmToken);
-        return ResponseEntity.ok(
-                CommonResponse.<Void>builder()
-                        .status(200)
-                        .message("FCM 토큰이 성공적으로 삭제되었습니다.")
-                        .data(null)
-                        .build()
-        );
+        return ResponseEntity.ok().build();
     }
 }
