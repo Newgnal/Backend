76bccd17502d97764f565cc1346272568bb9abb7
eomkyeongmun <eomkyeongmun@naver.com>
2025-06-27 19:19:55 +0900

    feat: 게시글 수정 기능 추가 (#15)
    * feat: 게시글 수정 기능 추가
    
    - PostUpdateRequestDto 생성
    - Post 엔티티에 update 메서드 추가
    - PostErrorCode에 수정 관련 에러 코드 추가
    
    * fix: controller/service 수정 및 에러 코드 개선
    
    - controller/service 메서드 인자: dto, userId, postId → dto, userId 로 수정
    - dto 클래스에 postId 필드 추가
    - 관련 로직에 맞게 에러 코드 수정
    
    * fix: controller,service 수정
    
    * fix: 게시글 수정 시 투표 엔티티 삭제 처리
    
    hasVote가 false로 변경되면 voteRepository.deleteAllByPost(post)로 해당 게시글의 투표 엔티티 삭제
    
    * feat(news): 테마 추가
    
    * feat: Post 엔티티에 thema 필드 추가 및 기존 내용 수정
    
    - Post 엔티티에 thema 필드 추가
    - 게시글 생성 및 수정시 thema 포함되도록 변경
    -Transactional 수정
    
    ---------
    
    Co-authored-by: limdodod <rosalim2001@naver.com>

--- src/main/java/com/tave/alarmissue/post/controller/PostController.java
+++ src/main/java/com/tave/alarmissue/post/controller/PostController.java
@@ -2,6 +2,7 @@ package com.tave.alarmissue.post.controller;
 
 import com.tave.alarmissue.auth.dto.request.PrincipalUserDetails;
 import com.tave.alarmissue.post.dto.request.PostCreateRequestDto;
+import com.tave.alarmissue.post.dto.request.PostUpdateRequestDto;
 import com.tave.alarmissue.post.dto.response.PostResponseDto;
 import com.tave.alarmissue.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -30,9 +31,16 @@ public class PostController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
 
     }
-    //게시글 조회
-
     //게시글 수정
+    @PatchMapping("/{postId}")
+    public ResponseEntity<PostResponseDto> updatePost(@PathVariable Long postId ,@RequestBody PostUpdateRequestDto dto , @AuthenticationPrincipal PrincipalUserDetails principal)
+    {
+        Long userId = principal.getUserId();
+        PostResponseDto responseDto = postService.updatePost(postId, dto, userId);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+
+    }
+
     //게시글 삭제
 
 }
