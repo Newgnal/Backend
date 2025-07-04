291209b89c92b3dad7073fe9fb55b1a23991ee65
limdodod <rosalim2001@naver.com>
2025-07-04 14:56:16 +0900

    feat(crawl): 삭제 코드 수정

--- src/main/java/com/tave/alarmissue/news/service/NewsDeleteService.java
+++ src/main/java/com/tave/alarmissue/news/service/NewsDeleteService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -15,19 +16,21 @@ public class NewsDeleteService {
 
     private final NewsRepository newsRepository;
 
-    // 매주 일요일 자정에 실행
-    @Scheduled(cron = "0 0 0 * * SUN")
+  //매주 월요일 00시 01분에 실행
+    @Scheduled(cron = "0 1 0 * * MON")
+    @Transactional
     public void deleteOldNews() {
         LocalDateTime oneWeekAgo = LocalDateTime.now().minusWeeks(1);
         int deletedCount = newsRepository.deleteByDateBefore(oneWeekAgo);
         log.info("[NewsDelete] 1주일 지난 뉴스 {}건 삭제 완료", deletedCount);
     }
 
-//    // 5분마다 실행 (테스트용)
+    // 5분마다 실행 (테스트용)
 //    @Scheduled(cron = "0 */5 * * * *")
+//    @Transactional
 //    public void deleteOldNews() {
-//        LocalDateTime oneWeekAgo = LocalDateTime.now().minusWeeks(1);
-//        int deletedCount = newsRepository.deleteByDateBefore(oneWeekAgo);
+//        LocalDateTime fiveMinAgo = LocalDateTime.now().minusMinutes(5);
+//        int deletedCount = newsRepository.deleteByDateBefore(fiveMinAgo);
 //        log.info("[NewsDelete] 5분 지난 뉴스 {}건 삭제 완료", deletedCount);
 //    }
 }
