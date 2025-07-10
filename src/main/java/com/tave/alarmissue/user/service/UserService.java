54ee56025ee10cb1e9029182fb3c7e50e24db666
limdodod <rosalim2001@naver.com>
2025-07-10 20:30:29 +0900

    feat(nickname): 닉네임 변경 구현

--- src/main/java/com/tave/alarmissue/user/service/UserService.java
+++ src/main/java/com/tave/alarmissue/user/service/UserService.java
@@ -1,10 +1,17 @@
 package com.tave.alarmissue.user.service;
 
 import com.tave.alarmissue.redis.service.RefreshTokenRedisService;
+import com.tave.alarmissue.user.domain.UserEntity;
 import com.tave.alarmissue.user.dto.response.LogoutResponse;
+import com.tave.alarmissue.user.dto.response.NicknameResponse;
+import com.tave.alarmissue.user.exception.UserException;
+import com.tave.alarmissue.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.tave.alarmissue.user.exception.UserErrorCode.USER_ID_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +19,23 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final RefreshTokenRedisService refreshTokenRedisService;
+    private final UserRepository userRepository;
 
     public LogoutResponse logout(Long userId) {
         boolean isDeleted = refreshTokenRedisService.deleteRefreshToken(userId);
         String message = isDeleted ? "Logout successful" : "Logout failed: User not found";
         return new LogoutResponse(message);
+    }
+
+    @Transactional
+    public NicknameResponse changeNickname(Long userId,String nickname) {
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(USER_ID_NOT_FOUND,"User not found: " + userId));
+
+        user.changeNickname(nickname);
+        userRepository.save(user);
+
+        return new NicknameResponse( user.getId(), user.getNickName());
     }
 
 }
