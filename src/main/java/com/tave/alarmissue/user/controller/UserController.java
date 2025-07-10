28b649c5c9c1fde200a6d1145d44a72e282648f1
limdodod <rosalim2001@naver.com>
2025-07-10 20:58:31 +0900

    feat(user): 회원 탈퇴 구현

--- src/main/java/com/tave/alarmissue/user/controller/UserController.java
+++ src/main/java/com/tave/alarmissue/user/controller/UserController.java
@@ -4,18 +4,17 @@ import com.tave.alarmissue.auth.dto.request.PrincipalUserDetails;
 import com.tave.alarmissue.user.dto.response.LogoutResponse;
 import com.tave.alarmissue.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user/v1")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -33,5 +32,18 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+
+    @DeleteMapping()
+    public ResponseEntity<Void> deleteUser(
+            @AuthenticationPrincipal PrincipalUserDetails principal
+    ) {
+
+        log.debug(" delete user with ID: {}", principal.getUserId());
+
+        Long userId = principal.getUserId();
+        userService.softDeleteUser(userId);
+
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/com/tave/alarmissue/user/service/UserService.java
+++ src/main/java/com/tave/alarmissue/user/service/UserService.java
@@ -1,16 +1,20 @@
 package com.tave.alarmissue.user.service;
 
 import com.tave.alarmissue.redis.service.RefreshTokenRedisService;
+import com.tave.alarmissue.user.domain.UserEntity;
 import com.tave.alarmissue.user.dto.response.LogoutResponse;
+import com.tave.alarmissue.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserService {
 
+    private final UserRepository userRepository;
     private final RefreshTokenRedisService refreshTokenRedisService;
 
     public LogoutResponse logout(Long userId) {
@@ -18,5 +22,15 @@ public class UserService {
         String message = isDeleted ? "Logout successful" : "Logout failed: User not found";
         return new LogoutResponse(message);
     }
+
+    // 회원 탈퇴 (softdelete)
+    @Transactional
+    public void softDeleteUser(Long userId) {
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(USER_ID_NOT_FOUND, "User not found: " + userId));
+
+        userRepository.delete(user);
+    }
+
 
 }
