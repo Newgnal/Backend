b4844b76036654d9552bfdd436597c3fc1afa71b
엄경문 <eomkyeongmun@naver.com>
2025-07-13 23:04:18 +0900

    refactor:중복되는 코드 private method로 변경

--- src/main/java/com/tave/alarmissue/post/exception/PostErrorCode.java
+++ src/main/java/com/tave/alarmissue/post/exception/PostErrorCode.java
@@ -25,8 +25,7 @@ public enum PostErrorCode implements ErrorCode {
     COMMENT_ID_MISMATCH(HttpStatus.BAD_REQUEST, "댓글 ID가 일치하지 않습니다."),
     REPLY_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN,"대댓글 삭제 권한이 없습니다."),
 
-    POST_REPORT_FORBIDDEN(HttpStatus.FORBIDDEN, "게시글 신고 권한이 없습니다."),
-    COMMENT_REPORT_FORBIDDEN(HttpStatus.FORBIDDEN, "댓글 신고 권한이 없습니다."),
+    CANNOT_REPORT(HttpStatus.FORBIDDEN, "신고 권한이 없습니다."),
     ALREADY_REPORTED(HttpStatus.FORBIDDEN, "이미 신고가 되었습니다.");
 
     private final HttpStatus httpStatus;
