9b786aa1d9bb131f32bc60e7e98edcfc02bf948d
limdodod <rosalim2001@naver.com>
2025-07-17 13:20:07 +0900

    refractor: 주석 삭제

--- src/main/java/com/tave/alarmissue/news/service/NewsCommentService.java
+++ src/main/java/com/tave/alarmissue/news/service/NewsCommentService.java
@@ -7,7 +7,7 @@ import com.tave.alarmissue.news.domain.enums.NewsVoteType;
 import com.tave.alarmissue.news.dto.request.NewsCommentRequestDto;
 import com.tave.alarmissue.news.dto.response.NewsCommentListResponseDto;
 import com.tave.alarmissue.news.dto.response.NewsCommentResponseDto;
-import com.tave.alarmissue.news.exceptions.NewsCommentException;
+import com.tave.alarmissue.news.exceptions.NewsException;
 import com.tave.alarmissue.news.repository.NewsCommentRepository;
 import com.tave.alarmissue.news.repository.NewsRepository;
 import com.tave.alarmissue.news.repository.NewsVoteRepository;
@@ -37,8 +37,8 @@ public class NewsCommentService {
 
     @Transactional
     public NewsCommentResponseDto createComment(NewsCommentRequestDto dto, Long userId, Long newsId){
-        UserEntity user=userRepository.findById(userId).orElseThrow(()->new NewsCommentException(USER_ID_NOT_FOUND,"사용자가 없습니다."));
-        News news=newsRepository.findById(newsId).orElseThrow(()->new NewsCommentException(NEWS_ID_NOT_FOUND,"newsId:"+newsId));
+        UserEntity user=userRepository.findById(userId).orElseThrow(()->new NewsException(USER_ID_NOT_FOUND,"사용자가 없습니다."));
+        News news=newsRepository.findById(newsId).orElseThrow(()->new NewsException(NEWS_ID_NOT_FOUND,"newsId:"+newsId));
 
 
         NewsVoteType newsVoteType = newsVoteRepository.findVoteTypeByNewsIdAndUserId(newsId,userId).orElse(null);
@@ -53,36 +53,19 @@ public class NewsCommentService {
 
     public NewsCommentListResponseDto getCommentsByNewsId(Long newsId) {
         List<NewsComment> comments=newsCommentRepository.findByNewsIdOrderByCreatedAtDesc(newsId);
-        News news = newsRepository.findById(newsId).orElseThrow(() -> new NewsCommentException(NEWS_ID_NOT_FOUND, "뉴스를 찾을 수 없습니다."));
+        News news = newsRepository.findById(newsId).orElseThrow(() -> new NewsException(NEWS_ID_NOT_FOUND, "뉴스를 찾을 수 없습니다."));
 
         Long totalCount=news.getCommentNum();
 
         return NewsCommentConverter.toCommentListResponseDto(newsId,totalCount,comments);
     }
 
-//    @Transactional
-//    public void deleteComment(Long commentId, Long userId, Long newsId) {
-//        UserEntity user=userRepository.findById(userId).orElseThrow(()->new NewsCommentException(USER_ID_NOT_FOUND,"사용자가 없습니다."));
-//        News news=newsRepository.findById(newsId).orElseThrow(()->new NewsCommentException(NEWS_ID_NOT_FOUND,"newsId:"+newsId));
-//
-//        //댓글 존재 확인
-//        NewsComment comment = newsCommentRepository.findById(commentId)
-//                .orElseThrow(() -> new NewsCommentException(COMMENT_ID_NOT_FOUND, "댓글을 찾을 수 없습니다."));
-//
-//        //작성자 본인인지 확인
-//        if(!comment.getUser().getId().equals(userId)){
-//            throw new NewsCommentException(UNAUTHORIZED_DELETE,"본인이 작성한 댓글만 삭제할 수 있습니다.");
-//        }
-//        //댓글 삭제
-//        newsCommentRepository.delete(comment);
-//        news.decrementCommentCount();
-//    }
 
     @Transactional
     public void deleteComment(Long commentId, Long userId) {
         // 해당 뉴스의 해당 사용자가 작성한 댓글만 조회
         NewsComment comment = newsCommentRepository.findByIdAndUserId(commentId, userId)
-                .orElseThrow(() -> new NewsCommentException(COMMENT_ID_NOT_FOUND,
+                .orElseThrow(() -> new NewsException(COMMENT_ID_NOT_FOUND,
                         "해당 뉴스에서 본인이 작성한 댓글을 찾을 수 없습니다."));
 
         News news = comment.getNews();
@@ -97,7 +80,7 @@ public class NewsCommentService {
     @Transactional
     public NewsCommentResponseDto updateComment(Long commentId, Long userId, NewsCommentRequestDto dto) {
         NewsComment comment = newsCommentRepository.findByIdAndNewsIdAndUserId(commentId, dto.getNewsId(), userId)
-                .orElseThrow(() -> new NewsCommentException(COMMENT_ID_NOT_FOUND,
+                .orElseThrow(() -> new NewsException(COMMENT_ID_NOT_FOUND,
                         "해당 뉴스에서 본인이 작성한 댓글을 찾을 수 없습니다."));
 
         //댓글 내용 업데이트
@@ -106,29 +89,6 @@ public class NewsCommentService {
         return NewsCommentConverter.toCommentResponseDto(comment);
 
     }
-
-
-
-
-//    @Transactional
-//    public NewsCommentResponseDto deleteCommentAndGetCount(Long commentId, Long userId){
-//        //댓글 조회
-//        NewsComment comment=newsCommentRepository.findById(commentId).orElseThrow(()->new NewsCommentException(COMMENT_ID_NOT_FOUND,"댓글을 찾을 수 없습니다."));
-//        Long newsId=comment.getNews().getId();
-//
-//        //작성자 본인 확인
-//        if(!comment.getUser().getId().equals(userId)){
-//            throw new NewsCommentException(UNAUTHORIZED_DELETE, "본인이 작성한 댓글만 삭제할 수 있습니다.");
-//        }
-//        //댓글 삭제
-//        newsCommentRepository.delete(comment);
-////        Long updatedCount=newsCommentRepository.countByNewsId(newsId);
-//        News news=comment.getNews();
-//        news.decrementCommentCount();
-//
-//        return NewsCommentConverter.toCommentResponseDto(newsId);
-//
-//    }
 
 
 }
