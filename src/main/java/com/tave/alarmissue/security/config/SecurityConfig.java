cad0fa40589c3eca5d73d04d79c51892e2ceedb3
ddhi7 <tjsanf211323@gmail.com>
2025-07-21 14:05:27 +0900

    fcm api white list 경로 추가

--- src/main/java/com/tave/alarmissue/security/config/SecurityConfig.java
+++ src/main/java/com/tave/alarmissue/security/config/SecurityConfig.java
@@ -43,8 +43,9 @@ public class SecurityConfig {
             "/search/v1/**",
             "/post/v1/detail/*",
             "/post/v1/list/**",
-            "/news/v1/comments/*",
-            "/ai/**"
+            "news/v1/comments/*",
+            "/ai/**",
+            "/fcm/v1/**"
     };
 
 
