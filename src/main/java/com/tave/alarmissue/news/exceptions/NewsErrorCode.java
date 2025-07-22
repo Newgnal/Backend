7b6a438a9cbf22dc023f8fd527a48c715cceeb6d
kim junsoo <kjs201105@naver.com>
2025-07-22 18:45:36 +0900

    fix: 다른 사람 댓글에 답글 달 수 있도록 수정

--- src/main/java/com/tave/alarmissue/news/exceptions/NewsErrorCode.java
+++ src/main/java/com/tave/alarmissue/news/exceptions/NewsErrorCode.java
@@ -15,7 +15,8 @@ public enum NewsErrorCode implements ErrorCode {
     NEWS_ID_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 뉴스가 존재하지 않습니다."),
     COMMENT_ID_NOT_FOUND(HttpStatus.BAD_REQUEST,"해당 댓글이 존재하지 않습니다."),
     VOTE_NOT_FOUND(HttpStatus.BAD_REQUEST,"투표가 존재하지 않습니다."),
-    UNAUTHORIZED_DELETE(HttpStatus.BAD_REQUEST,"댓글 삭제 권한이 없습니다."),
+    COMMENT_ACCESS_DENIED(HttpStatus.FORBIDDEN,"해당 댓글에 권한이 없습니다."),
+
     INVALID_REQUEST(HttpStatus.BAD_REQUEST,"대댓글에는 답글을 달 수 없습니다.");
 
     private final HttpStatus httpStatus;
